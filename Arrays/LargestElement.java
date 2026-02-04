9c5e203af5b97510c5bb4c061510f6fcbb450244
yasmin786-len <syedyasmin2005@gmail.com>
2026-02-04 10:18:47 +0530

    Adding folders into repo

---
+++ Arrays/LargestElement.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+class LargestElement{
+    public static int findLargest(int[] arr){
+        int largest=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        return largest;
+    }
+    public static int betterFindLargest(int[] arr){
+        Arrays.sort(arr);
+        return arr[arr.length-1];
+    }
+    public static int bruteForceFindLargest(int[] arr) {
+        
+        for (int i = 0; i < arr.length; i++) {
+            boolean isLargest = true;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    isLargest = false;
+                    break;
+                }
+            }
+            if (isLargest) return arr[i];
+        }
+        return -1; // should never reach
+    }
+    public static void main(String[] args) {
+        int arr[]={34, 67, 23, 89, 12, 90, 45};
+        int largest=findLargest(arr);
+        System.out.println("Optimal Approach:");
+        System.out.println("The largest element in the array is: " + largest);
+        largest=bruteForceFindLargest(arr);
+        System.out.println("Brute Force Approach:");
+        System.out.println("The largest element in the array is: " + largest);
+        largest=betterFindLargest(arr);
+        System.out.println("Better Approach:");
+        System.out.println("The largest element in the array is: " + largest);
+    }
+    
+    }
